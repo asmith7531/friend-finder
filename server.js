b220b1a6f84bf26a8806a82dd397b35355090126
Austin Smith <austinsmith7531@gmail.com>
2019-05-15 00:05:42 -0400

    I dont remember what i did but i want to work from my desktop so im pushing

---
+++ server.js
@@ -0,0 +1,48 @@
+//requiring dependencies
+var mysql = require("mysql");
+var express = require("express");
+var path = require("path");
+
+//sets up the express app
+var app = express();
+var PORT = 3000;
+
+//sets up the Express app to hadle data parsing
+app.use(express.urlencoded({
+  extended: true
+}));
+app.use(express.json());
+
+//allows us to serve static files
+app.use("/static", express.static(path.join(__dirname, "public")));
+
+var users = [{
+  routeName: "austins",
+  name: "Austin Smith",
+  phoneNumber: "900-200-3000",
+  email: "austins@gmail.com"
+}]
+
+// Routes
+//=================================================================
+
+app.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "/public/index.html"));
+})
+
+app.get("api/users", function () {
+  return res.json(users);
+})
+
+app.post("/api/users", function (req, res) {
+  //req.body hosts is equal to the JSON post sent from the user 
+  //this works because of our body parsing middleware
+  var newUser = req.body;
+  //Using RegEx Pattern to remove spaces from newUser
+  newUser.routeName = newUser.name.replace(/\s+/g, "").toLowerCase();
+});
+
+// Starts the server to begin listening
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+})
