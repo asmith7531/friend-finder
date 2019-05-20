6acbfa5fbe59ad5d24a20f716accb3d7c4c91677
Austin Smith <austinsmith7531@gmail.com>
2019-05-20 03:03:50 -0400

    added handlebars and started squashing all the new bugs that came along with that but it still needs more work.

--- db/seed.sql
+++ db/seed.sql
@@ -1,64 +1,70 @@
 USE friends_db;
 
-INSERT INTO users(name)
- VALUES(
-   'austin'
+INSERT INTO users
+  (name)
+VALUES(
+    'austin'
   );
 
-INSERT INTO users(name)
- VALUES(
-   'maddie'
+INSERT INTO users
+  (name)
+VALUES(
+    'maddie'
   );
 
-INSERT INTO users(name)
- VALUES(
-   'bob'
+INSERT INTO users
+  (name)
+VALUES(
+    'bob'
   );
 
-  INSERT INTO answers(
-    answer1,
-    answer2, 
-    answer3, 
-    answer4, 
-    answer5, 
-    user_id)
- VALUES(
-   'a',
-   'a',
-   'a',
-   'a',
-   'a',
-   1
+INSERT INTO answers
+  (
+  answer1,
+  answer2,
+  answer3,
+  answer4,
+  answer5,
+  user_id)
+VALUES(
+    'a',
+    'a',
+    'a',
+    'a',
+    'a',
+    1
   );
 
-   INSERT INTO answers(
-     answer1,
-     answer2,
-     answer3,
-     answer4,
-     answer5,
-     user_id)
- VALUES(
-   'b',
-   'b',
-   'b',
-   'b',
-   'b',
-   2
+INSERT INTO answers
+  (
+  answer1,
+  answer2,
+  answer3,
+  answer4,
+  answer5,
+  user_id)
+VALUES(
+    'b',
+    'b',
+    'b',
+    'b',
+    'b',
+    2
   );
 
-   INSERT INTO answers(
-     answer1, 
-     answer2, 
-     answer3, 
-     answer4, 
-     answer5, 
-     user_id)
- VALUES(
-   'c',
-   'c',
-   'c',
-   'c',
-   'c',
-   3
+INSERT INTO answers
+  (
+  answer1,
+  answer2,
+  answer3,
+  answer4,
+  answer5,
+  user_id)
+VALUES(
+    'c',
+    'c',
+    'c',
+    'c',
+    'c',
+    3
   );
